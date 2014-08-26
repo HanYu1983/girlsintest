ff91de1ef091d06281a62ff4e4266b54c15639a8
HanYu <hanyushome@gmail.com>
2014-08-26 23:59:39 +0800

    basic server

---
+++ goapp/FrontControl.go
@@ -0,0 +1,78 @@
+package hello
+
+import (
+    "fmt"
+    "net/http"
+    "reflect"
+    "encoding/json"
+    "strconv"
+)
+
+type ActionMap map[string]interface{}
+
+type DefaultResult struct {
+    Error string
+}
+
+func FrontControl(w http.ResponseWriter, r *http.Request, action ActionMap){
+    defer func(){
+        if x:= recover(); x != nil{
+            errmsg, _ := json.Marshal(DefaultResult{Error:x.(string)})
+            fmt.Fprintf(w, "%s", errmsg)
+        }
+    }()
+    r.ParseForm()
+    cmd := GetCommand(r, "nocmd")
+    result := Call(action, cmd, r)[0]
+    formatResult := result.Interface()
+    js, _ := json.Marshal(formatResult)
+    fmt.Fprintf(w, "%s",  js)
+}
+
+func GetCommand(r *http.Request, defcmd string)(cmd string){
+    cmd = defcmd
+    if len(r.Form["cmd"]) > 0 { cmd = r.Form["cmd"][0] }
+    return
+}
+
+func Call(m ActionMap, name string, params ... interface{}) (result []reflect.Value) {
+    PanicWhen(m[name] == nil, "no command "+name)
+    f := reflect.ValueOf(m[name])
+    in := make([]reflect.Value, len(params))
+    for k, param := range params {
+        in[k] = reflect.ValueOf(param)
+    }
+    result = f.Call(in)
+    return
+}
+
+func PanicWhen(cond bool, msg string){
+    if cond { panic(msg) }
+}
+
+func VerifyParam(r *http.Request, name string, fn func([]string)(bool, string)){
+    param := r.Form[name]
+    success, errmsg := fn(param)
+    PanicWhen( success == false, "parameter["+name+"] not right, "+errmsg )
+}
+
+func ParamNotNil() func([]string)(bool, string){
+    return func(paramlist []string) (bool, string){
+        return len(paramlist) > 0, "value should not be null"
+    }
+}
+
+func ParamInRange(min int, max int) func([]string) (bool, string){
+    return func(paramlist []string) (bool, string){
+        success, errmsg := ParamNotNil()(paramlist)
+        if success == false { return false, errmsg }
+        
+        value, err := strconv.ParseInt(paramlist[0], 10, 0)
+        errmsg = "value should be ["+strconv.Itoa(min)+"-"+strconv.Itoa(max)+"]"
+        if err == nil {
+            return int(value) >= min && int(value) <= max, errmsg
+        } else {
+            return false, errmsg
+        }
+    }
+}
--- goapp/hello.go
+++ goapp/hello.go
@@ -6,7 +6,16 @@ import (
 )
 
 func init() {
+    FrontControl := func(w http.ResponseWriter, r *http.Request){
+        FrontControl(w, r, 
+            ActionMap{
+                "RegisterUser":RegisterUser,
+                "QueryUser":QueryUser,
+            },
+        )
+    }
     http.HandleFunc("/", handler)
+    http.HandleFunc("/test", FrontControl)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {

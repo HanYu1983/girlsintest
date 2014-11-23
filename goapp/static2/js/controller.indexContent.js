167ee7346804bc223b465ab689f8a3ec9df7bc7d
vic <finalsteve@hotmail.com>
2014-11-24 01:05:47 +0800

    update new layout

--- goapp/static2/js/controller.indexContent.js
+++ goapp/static2/js/controller.indexContent.js
@@ -5,8 +5,38 @@ app.controller = app.controller || {};
 	var pkg = app.controller
 
 	function c_indexContent(view){
-		console.log( 'c_indexContentc_indexContent' );
 		//import sketchfab lib
+		
+		var sketchfabModule = window['sketchfab-iframe'];
+		var Sketchfab = sketchfabModule.Sketchfab;
+		
+		var sid = 'bb79930ee30944378ec957dcc6bed42d';
+		var iframe_3dIframe = view.get3dIframe();
+		console.log( 'iframe_3dIframe', iframe_3dIframe);
+		var options = {
+			autospin:.3,
+			ui_controls:0,
+			ui_infos:0,
+			nocamera: 1,
+			autostart: 0,
+			transparent: 0,
+			controls: 0,
+			watermark:0,
+			desc_button:0,
+			stop_button:0
+		};
+		
+		var iframeApi = new Sketchfab( iframe_3dIframe[0] );
+		sketchfabModule.Q.when( iframeApi.load( sid, options )).then( addListener ).fail( function( error ){
+			alert( error );
+		});
+		
+		function addListener( data ){
+			iframeApi.start();
+		}
+		
+		//import sketchfab lib
+		/*
 		var sketchfabModule = window['sketchfab-iframe'];
 		var Sketchfab = sketchfabModule.Sketchfab;
 		//local test
@@ -68,7 +98,7 @@ app.controller = app.controller || {};
 					});
 				});
 			}
-		}
+		}*/
 	}
 	
 	/*
--- goapp/static2/js/view.indexContent.js
+++ goapp/static2/js/view.indexContent.js
@@ -4,59 +4,13 @@ app.view = app.view || {};
 	var pkg = app.view
 	
 	function v_indexContent(elem){
-		var iframe_modelA = elem.find('#iframe_modelA' );
-		var iframe_modelB = elem.find('#iframe_modelB' );
-		var iframe_modelC = elem.find('#iframe_modelC' );
-		var mc_coverA = elem.find('#mc_coverA' );
-		var mc_coverB = elem.find('#mc_coverB' );
-		var mc_coverC = elem.find('#mc_coverC' );
-		var mc_forOver = elem.find('#mc_forOver' );
-		var mc_loading = elem.find('#mc_fakeloading' );
+		var iframe_3dIframe = elem.find( '#iframe_3dIframe' );
 		return {
-			getIframeASource:function( src ){
-				return iframe_modelA;
-			},
-			getIframeBSource:function( src ){
-				return iframe_modelB;
-			},
-			getIframeCSource:function( src ){
-				return iframe_modelC;
-			},
-			getCoverA:function(){
-				return mc_coverA;
-			},
-			getCoverB:function(){
-				return mc_coverB;
-			},
-			getCoverC:function(){
-				return mc_coverC;
-			},
-			getForOver:function(){
-				return mc_forOver;
-			},
-			getLoading:function(){
-				return mc_loading;
+			get3dIframe:function( src ){
+				return iframe_3dIframe;
 			}
 		}
 	}
-	/*
-	function v_indexContent(elem){
-		var btn_main = elem.find('#btn_main' );
-		var mc_forOver = elem.find( '#mc_forOver' );
-		var mc_iframes = elem.find( '#mc_iframes' );
-		return {
-			onBtnMainClick:function( fn ){
-				btn_main.click( fn );
-			},
-			getMcForOver:function(){
-				return mc_forOver;
-			},
-			getIframeContainer:function(){
-				return mc_iframes;
-			}
-		}
-	}
-	*/
 	pkg.v_indexContent = v_indexContent
 })();
 

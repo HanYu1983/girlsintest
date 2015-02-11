f4b0503f14624ff02a371221cc47288d6e985fa3
MacBook <justin@justNO11deMacBook-Pro.local>
2015-02-11 14:16:27 +0900

    no message

--- cljs_workspace/root/project.clj
+++ cljs_workspace/root/project.clj
@@ -4,11 +4,13 @@
   :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild {
     :builds [{
+        :id "cljstestproject.js"
         ; The path to the top-level ClojureScript source directory:
         :source-paths ["src"]
         ; The standard ClojureScript compiler options:
         ; (See the ClojureScript compiler documentation for details.)
         :compiler {
           :output-to "../../goapp/static4/js/main.js"  ; default: target/cljsbuild-main.js
-          :optimizations :whitespace
+          :output-dir "../../goapp/static4/js/cljslib"
+          :optimizations :none
           :pretty-print true}}]})
--- cljs_workspace/root/target/cljsbuild-compiler-0/test/core.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2268
-goog.provide('test.core');
-goog.require('cljs.core');
-test.core.eat = (function eat(food){return console.log("eat",food);
-});

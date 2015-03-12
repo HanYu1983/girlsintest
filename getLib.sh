d83e854d82ed1b49124b11de7a316ea29468ee98
HanYu <hanyushome@gmail.com>
2015-03-12 17:26:40 +0800

    add lib

---
+++ getLib.sh
@@ -0,0 +1,6 @@
+export GOPATH=/Users/hanyu/Documents/goapp_workspace/girlsintest/goapp/
+../go_appengine/goapp get github.com/nfnt/resize
+../go_appengine/goapp get github.com/tcard/functional
+../go_appengine/goapp get github.com/jordan-wright/email
+../go_appengine/goapp get github.com/pmylund/go-cache
+../go_appengine/goapp get github.com/rakyll/coop
--- goapp/src/hello.go
+++ goapp/src/hello.go
@@ -1,8 +1,7 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"net/http"
 	"lib/tool"
 	"app/test"
 	"app/handler"
@@ -39,33 +38,13 @@ func init() {
 		"QueryStreetModelPage": handler.QueryStreetModelPage,
 		"EditStreetModelPage": handler.EditStreetModelPage,
 	}
-  
-  
-  http.HandleFunc("/goapp/redirect", DefaultHandler)
-  http.HandleFunc("/goapp/cache", DefaultHandler2)
-  http.HandleFunc("/goapp/Func", tool.FrontControllerWith(actions, tool.AppEngineContextFactory))
+	http.HandleFunc("/goapp/Func", tool.FrontControllerWith(actions, tool.AppEngineContextFactory))
 	http.HandleFunc("/goapp/Page", tool.FrontControllerWith(pageActions, tool.AppEngineContextFactory))
 	http.HandleFunc("/goapp/Test", tool.FrontControllerWith(testActions, tool.AppEngineContextFactory))
 }
 func AdminPage(sys tool.ISystem)interface{}{
 	w := sys.GetResponse()
 	w.Header().Set("Content-Type", "text/html")
-    tool.TemplateWithFile("EditStreetModel", "app/tmpl/Admin.html").Execute(w, nil)
+		tool.TemplateWithFile("EditStreetModel", "app/tmpl/Admin.html").Execute(w, nil)
 	return tool.CustomView
-}
-		
-func DefaultHandler(w http.ResponseWriter, r *http.Request) {
-  http.Redirect(w, r, "/goapp/cache", http.StatusSeeOther)
-}
-
-func DefaultHandler2(w http.ResponseWriter, r *http.Request) {
-  etag := "xxade"
-  noneMatch := r.Header.Get("If-None-Match")
-  
-  if noneMatch == etag {
-    w.WriteHeader(http.StatusNotModified)
-  }
-  // browser看到ETag的話，在下一次呼叫同一個網址時，會在request中的header中If-None-Match代入這個欄位的值，所以可以用這個欄位來決定browser要不要從快取中抓取資料
-  w.Header().Set("ETag", etag)
-  fmt.Fprint(w, "Hello, world2!")
 }

a22edb038246a2f75d3c491e2c318c875f646ecc
HanYu <hanyushome@gmail.com>
2015-03-12 19:11:42 +0800

    add bed test framework

--- getLib.bat
+++ getLib.bat
@@ -4,3 +4,6 @@ set GOPATH=\Users\hanyu\Documents\goapp_workspace\girlsintest\goapp\
 ..\go_appengine\goapp get github.com\jordan-wright\email
 ..\go_appengine\goapp get github.com\pmylund\go-cache
 ..\go_appengine\goapp get github.com\rakyll\coop
+..\go_appengine\goapp get github.com\huandu\facebook
+..\go_appengine\goapp get github.com\asaskevich\govalidator
+..\go_appengine\goapp get github.com\franela\goblin
--- goapp/bdd/main_test.go
+++ goapp/bdd/main_test.go
@@ -1,32 +1,36 @@
 package bdd
 
 import (
+  "fmt"
   "testing"
   "appengine/aetest"
+  "github.com/franela/goblin"
 )
 
 func TestSuit(t *testing.T){
   c, err := aetest.NewContext(nil)
   if err != nil { t.Fatal(err) }
-  defer func(){
-    if x:= recover(); x!= nil {
-      t.Fatal("TestError:", x)
-    }
-  }()
   defer c.Close()
-  // Add your test function here, use Panic() to notify test error!
-  TestHandler(c)
-  ReadFile(c)
-  TestPromise(c)
-}
-
-func gen(nums ...int) <-chan int {
-    out := make(chan int)
-    go func() {
-        for _, n := range nums {
-            out <- n
-        }
-        close(out)
-    }()
-    return out
+  
+  g := goblin.Goblin(t)
+  Recover := func (){
+    if x:= recover(); x!= nil {
+      g.Fail( fmt.Sprintf("%s", x) )
+    }
+  }
+  
+  g.Describe("Numbers", func() {
+    g.It("handler", func() {
+      defer Recover()
+      TestHandler(c)
+    })
+    g.It("read file", func() {
+      defer Recover()
+      ReadFile(c)
+    })
+    g.It("promise", func() {
+      defer Recover()
+      TestPromise(c)
+    })
+  })
 }
